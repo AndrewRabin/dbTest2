e1094cffdf2cb0bf68ad5aeae1134245e2d7bb69
andrewr <andrewrabinmail@gmail.com>
2021-01-11 14:37:38 +0200

    cleaned

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-//var createError = require('http-errors');
-//var express = require('express');
-//var path = require('path');
-//var cookieParser = require('cookie-parser');
-//var logger = require('morgan');
-
-
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
-//var wikiRouter = require('./routes/wiki.js');
-//var catalogRouter = require('./routes/catalog');
-
-//var app = express();
-
-/////////////// DB /////////////////////////////////////////
-
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb+srv://dbAdmin:dbAdmin@cluster0.e1tui.mongodb.net/dbTest?retryWrites=true&w=majority';
 //var client = new MongoClient(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -37,82 +21,26 @@ var testSchema = new Schema(
 var testModel = mongoose.model('Test', testSchema);
 
 var testRecord = new testModel(
-  {
-      first_name: "Billy",
-      family_name: "Jobs",
-      age: 85
+    {
+        first_name: "Willy",
+        family_name: "Wong",
+        age: 85
   });
 
 testRecord.save(function (err) {
-  if (err) { return next(err); }
-  // Successful - redirect to new author record.
-  console.log("Success");
+    if (err) { return next(err); }
+
+    console.log("Success");
 });
 
 // the last one is not presented in a list!
 testModel.find(function f(err, results){
-  if (err) {
-    console.log("FIND ERROR");
-    return;
-  }
+    if (err) {
+        console.log("FIND ERROR");
+        return;
+    }
 
-  for(var resul of results)
+    for(var resul of results)
     console.log(resul.id);
 
 });
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// app.use('/wiki', wikiRouter);
-// app.use('/catalog', catalogRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-  
-//   //res.render('error', {message: "oooooops"});
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
-
-
-
-// run from terminal
-// $ENV:DEBUG = "LocalLibrary:*"; npm start
-//
-// compass
-// mongodb+srv://dbUser:<password>@cluster0.duzcf.mongodb.net/test
-
-//terminal
-// mongo "mongodb+srv://cluster0.duzcf.mongodb.net/dbLibrary" --username dbUser
